1ea6eaade052d156505b9c507f3695146a6f989f
Oscar Orellana <oscarore284@gmail.com>
2024-07-30 21:34:36 -0600

    feat: area calculator practice

---
+++ AreaCalculator-OM100123/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator_OM100123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
