9f2dc7022f2a3bad6d441f3776e85e1959f23c9d
Oscar Orellana <oscarore284@gmail.com>
2024-07-30 21:33:08 -0600

    First commit

---
+++ GuiaPractica1-OM100123/Kelvin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiaPractica1_OM100123
+{
+    internal class Kelvin
+    {
+        double celsius;
+
+        public Kelvin(double celsius)
+        {
+            this.celsius = celsius;
+        }
+
+        public double ConvertCelsiusToKelvin()
+        {
+            double k = celsius + 273.15;
+            return k;
+        }
+
+
+    }
+}
