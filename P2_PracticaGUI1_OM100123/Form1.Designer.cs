ec8e12c3f09023d06a9d563d540c4a1dfa303a66
Oscar Orellana <oscarore284@gmail.com>
2024-08-29 07:23:36 -0600

    subiendo todo

---
+++ P2_PracticaGUI1_OM100123/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace P2_PracticaGUI1_OM100123
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblAsignatura1 = new System.Windows.Forms.Label();
+            this.lblAsignatura2 = new System.Windows.Forms.Label();
+            this.lblAsignatura3 = new System.Windows.Forms.Label();
+            this.txtAsig1Lab1 = new System.Windows.Forms.TextBox();
+            this.txtAsig2Lab1 = new System.Windows.Forms.TextBox();
+            this.txtAsig3Lab1 = new System.Windows.Forms.TextBox();
+            this.txtAsign3Parcial1 = new System.Windows.Forms.TextBox();
+            this.txtAsign2Parcial1 = new System.Windows.Forms.TextBox();
+            this.txtAsign1Parcial1 = new System.Windows.Forms.TextBox();
+            this.lblLab1 = new System.Windows.Forms.Label();
+            this.lblParcial1 = new System.Windows.Forms.Label();
+            this.lblPromedio = new System.Windows.Forms.Label();
+            this.txtPromedio3 = new System.Windows.Forms.TextBox();
+            this.txtPromedio2 = new System.Windows.Forms.TextBox();
+            this.txtPromedio1 = new System.Windows.Forms.TextBox();
+            this.lblPromedioGeneral = new System.Windows.Forms.Label();
+            this.txtPromedioGeneral = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(373, 504);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(223, 81);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblAsignatura1
+            // 
+            this.lblAsignatura1.AutoSize = true;
+            this.lblAsignatura1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsignatura1.Location = new System.Drawing.Point(28, 125);
+            this.lblAsignatura1.Name = "lblAsignatura1";
+            this.lblAsignatura1.Size = new System.Drawing.Size(182, 32);
+            this.lblAsignatura1.TabIndex = 1;
+            this.lblAsignatura1.Text = "Asignatura 1:";
+            // 
+            // lblAsignatura2
+            // 
+            this.lblAsignatura2.AutoSize = true;
+            this.lblAsignatura2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsignatura2.Location = new System.Drawing.Point(28, 231);
+            this.lblAsignatura2.Name = "lblAsignatura2";
+            this.lblAsignatura2.Size = new System.Drawing.Size(182, 32);
+            this.lblAsignatura2.TabIndex = 2;
+            this.lblAsignatura2.Text = "Asignatura 2:";
+            // 
+            // lblAsignatura3
+            // 
+            this.lblAsignatura3.AutoSize = true;
+            this.lblAsignatura3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsignatura3.Location = new System.Drawing.Point(28, 345);
+            this.lblAsignatura3.Name = "lblAsignatura3";
+            this.lblAsignatura3.Size = new System.Drawing.Size(182, 32);
+            this.lblAsignatura3.TabIndex = 3;
+            this.lblAsignatura3.Text = "Asignatura 3:";
+            // 
+            // txtAsig1Lab1
+            // 
+            this.txtAsig1Lab1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAsig1Lab1.Location = new System.Drawing.Point(291, 122);
+            this.txtAsig1Lab1.Name = "txtAsig1Lab1";
+            this.txtAsig1Lab1.Size = new System.Drawing.Size(122, 39);
+            this.txtAsig1Lab1.TabIndex = 4;
+            // 
+            // txtAsig2Lab1
+            // 
+            this.txtAsig2Lab1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAsig2Lab1.Location = new System.Drawing.Point(291, 228);
+            this.txtAsig2Lab1.Name = "txtAsig2Lab1";
+            this.txtAsig2Lab1.Size = new System.Drawing.Size(122, 39);
+            this.txtAsig2Lab1.TabIndex = 5;
+            // 
+            // txtAsig3Lab1
+            // 
+            this.txtAsig3Lab1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAsig3Lab1.Location = new System.Drawing.Point(291, 338);
+            this.txtAsig3Lab1.Name = "txtAsig3Lab1";
+            this.txtAsig3Lab1.Size = new System.Drawing.Size(122, 39);
+            this.txtAsig3Lab1.TabIndex = 6;
+            // 
+            // txtAsign3Parcial1
+            // 
+            this.txtAsign3Parcial1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAsign3Parcial1.Location = new System.Drawing.Point(516, 338);
+            this.txtAsign3Parcial1.Name = "txtAsign3Parcial1";
+            this.txtAsign3Parcial1.Size = new System.Drawing.Size(122, 39);
+            this.txtAsign3Parcial1.TabIndex = 9;
+            // 
+            // txtAsign2Parcial1
+            // 
+            this.txtAsign2Parcial1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAsign2Parcial1.Location = new System.Drawing.Point(516, 228);
+            this.txtAsign2Parcial1.Name = "txtAsign2Parcial1";
+            this.txtAsign2Parcial1.Size = new System.Drawing.Size(122, 39);
+            this.txtAsign2Parcial1.TabIndex = 8;
+            // 
+            // txtAsign1Parcial1
+            // 
+            this.txtAsign1Parcial1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAsign1Parcial1.Location = new System.Drawing.Point(516, 122);
+            this.txtAsign1Parcial1.Name = "txtAsign1Parcial1";
+            this.txtAsign1Parcial1.Size = new System.Drawing.Size(122, 39);
+            this.txtAsign1Parcial1.TabIndex = 7;
+            // 
+            // lblLab1
+            // 
+            this.lblLab1.AutoSize = true;
+            this.lblLab1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLab1.Location = new System.Drawing.Point(270, 54);
+            this.lblLab1.Name = "lblLab1";
+            this.lblLab1.Size = new System.Drawing.Size(182, 32);
+            this.lblLab1.TabIndex = 10;
+            this.lblLab1.Text = "Laboratorio 1";
+            // 
+            // lblParcial1
+            // 
+            this.lblParcial1.AutoSize = true;
+            this.lblParcial1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblParcial1.Location = new System.Drawing.Point(515, 54);
+            this.lblParcial1.Name = "lblParcial1";
+            this.lblParcial1.Size = new System.Drawing.Size(132, 32);
+            this.lblParcial1.TabIndex = 11;
+            this.lblParcial1.Text = "Parcial 1 ";
+            // 
+            // lblPromedio
+            // 
+            this.lblPromedio.AutoSize = true;
+            this.lblPromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPromedio.Location = new System.Drawing.Point(771, 54);
+            this.lblPromedio.Name = "lblPromedio";
+            this.lblPromedio.Size = new System.Drawing.Size(136, 32);
+            this.lblPromedio.TabIndex = 12;
+            this.lblPromedio.Text = "Promedio";
+            this.lblPromedio.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtPromedio3
+            // 
+            this.txtPromedio3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPromedio3.Location = new System.Drawing.Point(777, 334);
+            this.txtPromedio3.Name = "txtPromedio3";
+            this.txtPromedio3.Size = new System.Drawing.Size(122, 39);
+            this.txtPromedio3.TabIndex = 15;
+            // 
+            // txtPromedio2
+            // 
+            this.txtPromedio2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPromedio2.Location = new System.Drawing.Point(777, 224);
+            this.txtPromedio2.Name = "txtPromedio2";
+            this.txtPromedio2.Size = new System.Drawing.Size(122, 39);
+            this.txtPromedio2.TabIndex = 14;
+            // 
+            // txtPromedio1
+            // 
+            this.txtPromedio1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPromedio1.Location = new System.Drawing.Point(777, 118);
+            this.txtPromedio1.Name = "txtPromedio1";
+            this.txtPromedio1.Size = new System.Drawing.Size(122, 39);
+            this.txtPromedio1.TabIndex = 13;
+            // 
+            // lblPromedioGeneral
+            // 
+            this.lblPromedioGeneral.AutoSize = true;
+            this.lblPromedioGeneral.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPromedioGeneral.Location = new System.Drawing.Point(776, 408);
+            this.lblPromedioGeneral.Name = "lblPromedioGeneral";
+            this.lblPromedioGeneral.Size = new System.Drawing.Size(123, 32);
+            this.lblPromedioGeneral.TabIndex = 16;
+            this.lblPromedioGeneral.Text = "General";
+            // 
+            // txtPromedioGeneral
+            // 
+            this.txtPromedioGeneral.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPromedioGeneral.Location = new System.Drawing.Point(777, 443);
+            this.txtPromedioGeneral.Name = "txtPromedioGeneral";
+            this.txtPromedioGeneral.Size = new System.Drawing.Size(122, 39);
+            this.txtPromedioGeneral.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 625);
+            this.Controls.Add(this.txtPromedioGeneral);
+            this.Controls.Add(this.lblPromedioGeneral);
+            this.Controls.Add(this.txtPromedio3);
+            this.Controls.Add(this.txtPromedio2);
+            this.Controls.Add(this.txtPromedio1);
+            this.Controls.Add(this.lblPromedio);
+            this.Controls.Add(this.lblParcial1);
+            this.Controls.Add(this.lblLab1);
+            this.Controls.Add(this.txtAsign3Parcial1);
+            this.Controls.Add(this.txtAsign2Parcial1);
+            this.Controls.Add(this.txtAsign1Parcial1);
+            this.Controls.Add(this.txtAsig3Lab1);
+            this.Controls.Add(this.txtAsig2Lab1);
+            this.Controls.Add(this.txtAsig1Lab1);
+            this.Controls.Add(this.lblAsignatura3);
+            this.Controls.Add(this.lblAsignatura2);
+            this.Controls.Add(this.lblAsignatura1);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Cálculo de promedio de asignaturas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblAsignatura1;
+        private System.Windows.Forms.Label lblAsignatura2;
+        private System.Windows.Forms.Label lblAsignatura3;
+        private System.Windows.Forms.TextBox txtAsig1Lab1;
+        private System.Windows.Forms.TextBox txtAsig2Lab1;
+        private System.Windows.Forms.TextBox txtAsig3Lab1;
+        private System.Windows.Forms.TextBox txtAsign3Parcial1;
+        private System.Windows.Forms.TextBox txtAsign2Parcial1;
+        private System.Windows.Forms.TextBox txtAsign1Parcial1;
+        private System.Windows.Forms.Label lblLab1;
+        private System.Windows.Forms.Label lblParcial1;
+        private System.Windows.Forms.Label lblPromedio;
+        private System.Windows.Forms.TextBox txtPromedio3;
+        private System.Windows.Forms.TextBox txtPromedio2;
+        private System.Windows.Forms.TextBox txtPromedio1;
+        private System.Windows.Forms.Label lblPromedioGeneral;
+        private System.Windows.Forms.TextBox txtPromedioGeneral;
+    }
+}
+
