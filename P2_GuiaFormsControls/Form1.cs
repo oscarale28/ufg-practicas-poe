bde3c021caec53dfcc3965ba3a0ab1cf03aaa9a4
Oscar Orellana <oscarore284@gmail.com>
2024-09-10 21:09:34 -0600

    guia forms + .gitignore

---
+++ P2_GuiaFormsControls/Form1.cs
@@ -0,0 +1,42 @@
+﻿using P2_GuiaFormsControls.Forms.Controles;
+using P2_GuiaFormsControls.Forms.ListBoxComboBox;
+using P2_GuiaFormsControls.Forms.PruebaLabel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P2_GuiaFormsControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjemplo1_Click(object sender, EventArgs e)
+        {
+            // show the Controles form
+            Controles controles = new Controles();
+            controles.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PruebaLabel pruebaLabel = new PruebaLabel();
+            pruebaLabel.Show();
+        }
+
+        private void btnDisplayListBoxComboBox_Click(object sender, EventArgs e)
+        {
+            ListBoxComboBox listBoxComboBox = new ListBoxComboBox();
+            listBoxComboBox.Show();
+        }
+    }
+}
---
+++ P2_GuiaFormsControls/Forms/Controles/Controles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P2_GuiaFormsControls.Forms.Controles
+{
+    public partial class Controles : Form
+    {
+        public Controles()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplayTextboxes_Click(object sender, EventArgs e)
+        {
+            TextBox txtBox1 = new TextBox
+            {
+                Location = new Point(50, 50),
+                Size = new Size(250, 20),
+                Font = new Font("Arial", 12)
+            };
+            TextBox txtBox2 = new TextBox
+            {
+                Location = new Point(50, 100),
+                Size = new Size(250, 20),
+                Font = new Font("Arial", 12)
+            };
+            this.Controls.Add(txtBox1);
+            this.Controls.Add(txtBox2);
+        }
+    }
+}
