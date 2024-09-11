98cc3fdd09a3b81a68f2440156d893b1e4afbed6
Oscar Orellana <oscarore284@gmail.com>
2024-09-10 21:16:44 -0600

    stop tracking

---
+++ AreaCalculator-OM100123/classes/Cuadrado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator_OM100123.classes
+{
+    internal class Cuadrado : FiguraGeometrica
+    {
+
+        double lado;
+
+        public Cuadrado(double lado)
+        {
+            this.lado = lado;
+        }
+
+        public override double CalculateArea()
+        {
+            double area = Math.Pow(this.lado, 2);
+            return area;
+        }
+    }
+}
---
+++ AreaCalculator-OM100123/classes/FiguraGeometrica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator_OM100123
+{
+    internal class FiguraGeometrica
+    {
+        double area;
+
+        public virtual double CalculateArea()
+        {
+            return area;
+        }
+    }
+}
